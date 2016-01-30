4059abd6b4ace05126a0078836ab425d1445d0fd
eduardo <eduardo.alvarez@justdigital.com.br>
2016-01-30 16:03:04 -0200

    css e js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
-  alert('sadasd');
-});
+  var mins=1;
+  var secs = mins*60;
+  var timerInterv;
+  $("#keynote").click(function(){
+    zerar();
+    mins=5;
+  });
+  $(".proximo").click(function(){
+    zerar();
+    var timerInterv = window.setInterval(doCountDown, 1000);
+    var outMins,outSecs
+  })
+
+
+
+  function doCountDown()
+  {
+      --secs;
+      if (secs<=0)
+      { outMins=outSecs=0;
+        clearInterval(timerInterv);
+        $("#segundo").html("00");
+        $(".cronometro").addClass("danger blink");
+      return;
+      }
+      outMins = parseInt(secs/60);
+      outSecs = secs%60;
+
+      $("#minuto").html(outMins);
+      $("#segundo").html(outSecs);
+
+
+  }
+
+  function zerar(){
+    for (var i = 1; i < 99999; i++)
+        window.clearInterval(i);
+  }
+})
